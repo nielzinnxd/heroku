6432b6f2c2a3b6ad9dc2f8b382c7165a2e96c139
nielzinnxd <daniel_rocha2009@hotmail.com>
2017-05-03 15:46:25 -0300

    Update Message.java

--- src/main/java/br/com/caelum/ichat/model/Message.java
+++ src/main/java/br/com/caelum/ichat/model/Message.java
@@ -3,11 +3,15 @@ package br.com.caelum.ichat.model;
 public class Message {
 	private String text;
 	private Integer id;
+	private String user;
 
 	public String getText() {
 		return text;
 	}
 
+	public String getUser(){
+		return user;
+	}
 	public Integer getId() {
 		return id;
 	}
